d76b67c5b36f8ab2c3a1e54d29a45abad19edc06
Zeyad Al-Houssainy <zeyad-houssainy@hotmail.com>
2024-12-17 22:29:28 +0200

    Webpack is ready to work

---
+++ game_nexus/webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	mode: 'development',
+	devServer: {
+		hot: true,
+		port: 9000,
+		compress: true,
+		static: {
+			directory: path.join(__dirname, './dist'),
+		},
+		historyApiFallback: true,
+	},
+	plugins: [new HtmlWebpackPlugin()],
+};
