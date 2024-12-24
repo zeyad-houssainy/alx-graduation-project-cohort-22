c53245c94c2b976d20b2b832aae2991c99fbac0e
Zeyad Al-Houssainy <zeyad-houssainy@hotmail.com>
2024-12-24 23:21:08 +0200

    fast_commit

--- game_nexus/src/App/App.js
+++ game_nexus/src/App/App.js
@@ -4,8 +4,13 @@ import React from 'react';
 import "./App.css"
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+
 // Font import
 import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// Material UI
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 
 
@@ -17,6 +22,16 @@ class App extends React.Component {
       		<h1>Hi from zeyad i'll add the components here</h1>
           <body>
             <h3>Body section will be here</h3>
+            <div>
+              <ButtonGroup variant="contained" aria-label="Basic button group">
+                <Button>One</Button>
+                <Button color="primary">Two</Button>
+                <Button color="primary">Three</Button>
+                <Button variant="contained" color="primary"> Secondary </Button>
+                <Button variant="outlined" color="primary"> Primary </Button>
+              </ButtonGroup>
+              {/* Testing Material UI Features */}
+            </div>
           </body>
           <Footer />
 		</>
