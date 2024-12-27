eae9e508e817b52e90b918b9760af5ec5b75f269
Zeyad Al-Houssainy <zeyad-houssainy@hotmail.com>
2024-12-27 04:10:44 +0200

    Products list compnent + Material UI common components

--- game_nexus/src/App/App.js
+++ game_nexus/src/App/App.js
@@ -1,10 +1,13 @@
 //React Imports
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, Outlet } from 'react-router-dom';
 //component Files import
 import "./App.css"
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import ProductList from '../Pages/ProductList/ProductList';
+import NoPage from '../Pages/NoPage/NoPage';
 
 // Font import
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -15,14 +18,18 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 
 
 
+
 class App extends React.Component {
   render() {
     return (
-      <>
-        <Header />
-        <ProductList />
-        <Footer />
-      </>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<NoPage />} />
+            <Route path="/products" element={<ProductList />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
     );
   }
 }
--- game_nexus/src/components/Header/NavBar/NavBar.js
+++ game_nexus/src/components/Header/NavBar/NavBar.js
@@ -2,18 +2,27 @@
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import './Navbar.css';
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  
+}
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="navbar-menu">
-        <li><a href="#" className="navbar-link">Consoles</a></li>
-        <li><a href="#" className="navbar-link">Games</a></li>
-        <li><a href="#" className="navbar-link">Figures & Toys</a></li>
-        <li><a href="#" className="navbar-link">Streams</a></li>
-      </ul>
-      <SearchBar /> {/* Include the SearchBar component */}
-    </nav>
+    <>
+      <nav className="navbar">
+        <ul className="navbar-menu">
+          <li><Link to="/">Consoles</Link></li>
+          <li><Link to="/">Games</Link></li>
+          <li><Link to="/">Figures & Toys</Link></li>
+          <li><Link to="/">Streams</Link></li>
+        </ul>
+        <SearchBar /> {/* Include the SearchBar component */}
+      </nav>
+
+      <Outlet />
+    </>
   );
 };
 
